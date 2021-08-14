2ffc5a2f5e5d979fa029f7ff5c67fe66569f6601
Bryce Hoover <brycehoover@Bryces-MacBook-Pro.local>
2021-08-13 20:56:18 -0500

    Expanded Search component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import React from 'react';
 import Search from './components/Search';
 
 const App = () => {
-    const items = [
-        {
-            title: 'What is React?',
-            content: 'React is a front-end Javascript framework'
-        },
-        {
-            title: 'Why use React?',
-            content: 'React is a favorite JS library among engineers'
-        },
-        {
-            title: 'How do you use React?',
-            content: 'You use React by creating components'
-        }
-    ]
+    // const items = [
+    //     {
+    //         title: 'What is React?',
+    //         content: 'React is a front-end Javascript framework'
+    //     },
+    //     {
+    //         title: 'Why use React?',
+    //         content: 'React is a favorite JS library among engineers'
+    //     },
+    //     {
+    //         title: 'How do you use React?',
+    //         content: 'You use React by creating components'
+    //     }
+    // ]
     return (
         <div>
            {/* <Accordion items={items}/> */}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <div>
-            Search
+            <div className="ui form">
+                <div className="field">
+                    <label htmlFor="searchBar">Enter search term: </label>
+                    <input id="searchBar" onChange={e => setSearchTerm(e.target.value)} value={searchTerm} className="input"></input>
+                </div>
+            </div>
         </div>
     )
 }
