46aae494df67aad896bbd8eb675c5e2a7dfa3e8b
Bryce Hoover <bryhoover11@gmail.com>
2021-08-14 20:32:52 -0500

    Added axios, setup wiki api functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState('');
+
+
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: searchTerm
+                }
+            });
+        };
+
+        search();
+    }, [searchTerm]);
 
     return (
         <div>
