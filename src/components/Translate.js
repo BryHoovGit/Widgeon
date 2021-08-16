3e04dcf87c5d7b9b350157c1a725ae42087c0a63
Bryce Hoover <bryhoover11@gmail.com>
2021-08-16 10:47:41 -0500

    Expanded Translate component, initialized state

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,9 +18,22 @@ const options = [
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
 
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="translate-text" className="label">
+            Enter Text
+          </label>
+          <input
+            id="translate-text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+      </div>
       <Dropdown
         label="Select a Language"
         selected={language}
