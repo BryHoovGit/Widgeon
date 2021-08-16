58fe26c3f02b5cdda540ef4518d77c88bbe8302f
Bryce Hoover <bryhoover11@gmail.com>
2021-08-15 20:35:04 -0500

    Fixed edge case for Dropdown with useRef

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Drowpdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -18,8 +33,9 @@ const Drowpdown = ({ options, selected, onSelectedChange }) => {
       </div>
     );
   });
+
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
